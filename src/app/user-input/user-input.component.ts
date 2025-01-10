76f8164fb12b6938fe09e8a131c4de8b7aeba5ed
Keke <67804511+kesia-feitosa@users.noreply.github.com>
2025-01-10 10:52:54 -0500

    Create model to suport values and create function to calculate

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,13 +1,29 @@
-import { Component, Input } from '@angular/core';
-import { User } from './user.model';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import type { InvestmentInput } from './investment-input.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  //@Input({ required: true }) user!: User;
+
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '5';
+  duration = '10';
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.initialInvestment,
+      duration: +this.duration,
+      expectedReturn: +this.expectedReturn,
+      annualInvestment: +this.annualInvestment
+    });
+  }
 }
